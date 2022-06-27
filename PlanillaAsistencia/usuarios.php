944db9c49e541e090e4687e9c51035797f791cc2
Andres Mastrolorenzo <afmastro@gmail.com>
2022-06-26 22:27:08 -0300

    errores en el script del php

--- PlanillaAsistencia/usuarios.php
+++ PlanillaAsistencia/usuarios.php
@@ -1,29 +1,35 @@
 <!-- conexion a la base de datos -->
 <?php
-$link = mysql_connect("localhost","root","") or die ("<h2>Nada por aqui!!</h2>");
-$db = mysql_select_db("usuarios",$link) or die ("<h2>Nada por alla</h2>");
+$enlace = mysqli_connect("localhost","root","");
+if (!$enlace) {
+    die('Nada por aqui: '.mysqli_error());
+} 
+$db_seleccionada = mysqli_select_db('usuarios', $enlace);
+if (!$enlace){
+    die ('Nada por alla: '.mysqli_error());
+} 
 
 // <!-- envia los valores del formulario a la base de datos -->
-$nombre=$_POST["nombre"];
-$apellido=$_POST["apellido"];
-$comunidad=$_POST["comunidad"];
-$cargo=$_POST["cargo"];
-$usuario=$_POST["usuario"];
-$Pass=$_POST["contraseña"];
-$RPass=$_POST["confirmar contraseña"];
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$comunidad = $_POST["comunidad"];
+$cargo = $_POST["cargo"];
+$usuario = $_POST["usuario"];
+$Pass = $_POST["contrasenia"];
+$RPass = $_POST["confirmar contrasenia"];
 
 // <!-- confirmacion de contraseña -->
-if ($Pass != $rPass){
-    die (No son igules algo esta mal)
-}
+if ($Pass != $Pass)
+    die ("<h2>Algo esta mal<h2>");
 
-// <!-- encriptar contraseñas -->
+
+// <!-- encriptar contraseñas 
 $contraseña = md5 ($Pass);
 
 // <!-- ingresa la info a la tabla -->
-mysql_query("INSERT INTO registro VALUES ('$nombre','$apellido','$comunidad','$cargo','$usuario','$contraseña')",$link) or die ("<h2>No se mando nada</h2>");
+$sql = "insert into registro values ('$nombre','$apellido','$comunidad','$cargo','$usuario','$contraseña'),$enlace)" or die ("<h2>No se mando nada</h2>");
 
 // <!-- redirige a la pagina -->
-echo '
-<a href="lista.html"></a>';
+echo 
+'por que no anda';
 ?>
